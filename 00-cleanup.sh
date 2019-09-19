30a55bc08400a152b3d0eab6e6ce37ce4beb5ab2
tarlety <tarlety@users.noreply.github.com>
2019-09-19 23:27:59 +0800

    fix ssh forwarding.

--- 00-cleanup.sh
+++ 00-cleanup.sh
@@ -8,3 +8,21 @@ do
 done
 
 VBoxManage dhcpserver remove --netname intnet
+
+for t in master1,10.13.13.101,2201 worker1,10.13.13.102,2202 worker2,10.13.13.103,2203 worker3,10.13.13.104,2204 worker4,10.13.13.105,2205
+do
+	IFS=","
+	set -- $t
+	NODE=$1
+	NODEIP=$2
+	FORWARD_PORT=$3
+
+	echo "Disable forwarding localhost:${FORWARD_PORT} to ${NODE}:22"
+	VBoxManage controlvm ${NODE} natpf1 delete "ssh-${NODE}"
+	sudo iptables -t nat -D PREROUTING -p tcp \
+		-d ${NODEIP} --dport 22 \
+		-j DNAT --to-destination 127.0.0.1:${FORWARD_PORT}
+	sudo iptables -t nat -D OUTPUT -p tcp \
+		-d ${NODEIP} --dport 22 \
+		-j DNAT --to-destination 127.0.0.1:${FORWARD_PORT}
+done
--- 03-enable-ssh-forwarding.sh
+++ 03-enable-ssh-forwarding.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-echo "Enable forwarding localhost:2201 to ${NODE}:22"
-VBoxManage controlvm ${NODE} natpf1 "ssh-${NODE},tcp,127.0.0.1,2201,22"
-sudo iptables -t nat -A PREROUTING -p tcp \
-	-d 10.13.13.101 --dport 22 \
-	-j DNAT --to-destination 127.0.0.1:2201 \
-sudo iptables -t nat -A OUTPUT -p tcp \
-	-d 10.13.13.101 --dport 22 \
-	-j DNAT --to-destination 127.0.0.1:2201 \
+for t in master1,10.13.13.101,2201 worker1,10.13.13.102,2202 worker2,10.13.13.103,2203 worker3,10.13.13.104,2204 worker4,10.13.13.105,2205
+do
+	IFS=","
+	set -- $t
+	NODE=$1
+	NODEIP=$2
+	FORWARD_PORT=$3
 
+	echo "Enable forwarding localhost:${FORWARD_PORT} to ${NODE}:22"
+	echo VBoxManage controlvm ${NODE} natpf1 "ssh-${NODE},tcp,127.0.0.1,${FORWARD_PORT},,22"
+	sudo iptables -t nat -A PREROUTING -p tcp \
+		-d ${NODEIP} --dport 22 \
+		-j DNAT --to-destination 127.0.0.1:${FORWARD_PORT}
+	sudo iptables -t nat -A OUTPUT -p tcp \
+		-d ${NODEIP} --dport 22 \
+		-j DNAT --to-destination 127.0.0.1:${FORWARD_PORT}
+done
