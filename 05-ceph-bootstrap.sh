5910c623a4ca26100c764f7616809d7e48cad3e5
tarlety <tarlety@users.noreply.github.com>
2019-09-24 22:09:23 +0800

    day9

--- 05-ceph-bootstrap.sh
+++ 05-ceph-bootstrap.sh
@@ -15,6 +15,8 @@ MON3IP=10.13.13.103
 MONS=${MON1},${MON2},${MON3}
 MONSIP=${MON1IP},${MON2IP},${MON3IP}
 
+PGNUM=64
+
 # Bootstrap Ceph by MON1
 
 ssh ${MON1} "
@@ -99,5 +101,68 @@ do
 			-e CEPH_PUBLIC_NETWORK=$NETWORK \
 			--name mon \
 			${DOCKERIMAGE} mon
+		"
+done
+
+# Bootstrap mgr/osd/mds daemon
+
+for NODE in ${MON1} ${MON2} ${MON3}
+do
+	ssh ${NODE} "
+		docker run -d --net=host \
+			--restart=always \
+			-v /etc/ceph:/etc/ceph \
+			-v /var/log/ceph/:/var/log/ceph/ \
+			-v /var/lib/ceph/mgr:/var/lib/ceph/mgr \
+			-v /var/lib/ceph/bootstrap-mds:/var/lib/ceph/bootstrap-mds \
+			-v /var/lib/ceph/bootstrap-osd:/var/lib/ceph/bootstrap-osd \
+			-v /var/lib/ceph/bootstrap-rbd:/var/lib/ceph/bootstrap-rbd \
+			-v /var/lib/ceph/bootstrap-rgw:/var/lib/ceph/bootstrap-rgw \
+			--name mgr \
+			${DOCKERIMAGE} mgr
+		"
+
+	for DEV in /dev/sdb /dev/sdc
+	do
+		OSDNAME=osd${DEV//\//-}
+		ssh ${NODE} "
+			docker run --rm --privileged=true \
+				-v /dev/:/dev/ \
+				-e OSD_DEVICE=$DEV \
+				${DOCKERIMAGE} zap_device
+			"
+		ssh ${NODE} "
+			docker run -d --net=host \
+			    --pid=host \
+			    --restart=always \
+			    --privileged=true \
+			    -v /etc/ceph:/etc/ceph \
+			    -v /var/log/ceph/:/var/log/ceph/ \
+			    -v /var/lib/ceph/bootstrap-mds:/var/lib/ceph/bootstrap-mds \
+			    -v /var/lib/ceph/bootstrap-osd:/var/lib/ceph/bootstrap-osd \
+			    -v /var/lib/ceph/bootstrap-rbd:/var/lib/ceph/bootstrap-rbd \
+			    -v /var/lib/ceph/bootstrap-rgw:/var/lib/ceph/bootstrap-rgw \
+			    -v /dev/:/dev/ \
+			    -e OSD_DEVICE=$DEV \
+			    --name $OSDNAME \
+			    ${DOCKERIMAGE} osd
+			"
+	done
+
+	ssh ${NODE} "
+		docker run -d --net=host \
+			--restart=always \
+			-v /etc/ceph:/etc/ceph \
+			-v /var/log/ceph/:/var/log/ceph/ \
+			-v /var/lib/ceph/mds:/var/lib/ceph/mds \
+			-v /var/lib/ceph/bootstrap-mds:/var/lib/ceph/bootstrap-mds \
+			-v /var/lib/ceph/bootstrap-osd:/var/lib/ceph/bootstrap-osd \
+			-v /var/lib/ceph/bootstrap-rbd:/var/lib/ceph/bootstrap-rbd \
+			-v /var/lib/ceph/bootstrap-rgw:/var/lib/ceph/bootstrap-rgw \
+			-e CEPHFS_CREATE=1 \
+			-e CEPHFS_DATA_POOL_PG=$PGNUM \
+			-e CEPHFS_METADATA_POOL_PG=$PGNUM \
+			--name mds \
+			${DOCKERIMAGE} mds
 		"
 done
